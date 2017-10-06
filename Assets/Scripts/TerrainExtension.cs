4f3c14a2d15844ea42b21107d7af2d5782f6a3cf
Alequis <alequis01@gmail.com>
2017-10-06 15:37:19 -0300

    Add project with terrain and some tests

---
+++ Assets/Scripts/TerrainExtension.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TerrainExtension {
+
+    public static void LoadTerrain(string fileName, TerrainData terrainData) {
+        //the file location (folder/fileName.raw) 
+        fileName = Application.dataPath + fileName;
+
+        //get the terrain heightmap width and height.
+        int x = terrainData.heightmapWidth;
+        int y = terrainData.heightmapHeight;
+
+        //create a float array with the terrain x, y size
+        float[,] data = new float[x, y];
+
+        using (var file = System.IO.File.OpenRead(fileName))
+        using (var reader = new System.IO.BinaryReader(file)) {
+            for (int i = 0; i < x; i++) {
+                for (int j = 0; j < y; j++) {
+                    float v = (float)reader.ReadUInt16() / 0xFFFF;
+                    data[i, j] = v;
+                }
+            }
+        }
+        terrainData.SetHeights(0, 0, data);
+    }
+
+}
