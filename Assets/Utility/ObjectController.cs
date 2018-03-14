55b0ab69f1216149cb1ab7632634d08d7ff15aa6
Unknown <alequis01@gmail.com>
2018-03-14 17:38:21 -0300

    Add object destroy

---
+++ Assets/Utility/ObjectController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(lifeTime(3));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private IEnumerator lifeTime (float time) {
+		yield return new WaitForSeconds(time);
+		Destroy(gameObject);
+	}
+}
